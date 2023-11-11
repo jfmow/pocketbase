d9b14e3d57f516022201ed307dfd3349a2c02407
sudsymoss <james1mowat@gmail.com>
2023-11-12 10:03:33 +1300

    re build 2

--- installer/main.go
+++ installer/main.go
@@ -26,18 +26,29 @@ func main() {
 	runningAppPath := filepath.Join(parentDir, "base")
 
 	fmt.Println("Running App Path:", runningAppPath)
+
+	// Remove the existing executable
 	err = os.Remove(runningAppPath)
 	if err != nil {
 		log.Fatal("Failed to delete current exe: ", err)
 	}
+
+	// Copy the new executable
 	copyFile(newBaseDir, runningAppPath)
+
+	// Set permissions for the new executable
+	err = os.Chmod(runningAppPath, 0755)
+	if err != nil {
+		log.Fatal("Failed to set permissions for the new exe: ", err)
+	}
+
+	// Run the new executable
 	cmd := exec.Command(runningAppPath)
 
 	err = cmd.Run()
 	if err != nil {
 		log.Fatal(err)
 	}
-
 }
 
 func copyFile(sourcePath, destinationPath string) error {
