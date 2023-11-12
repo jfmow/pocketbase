cb9b977c304e0be9cf179383ee7746e2e5b4282e
sudsymoss <james1mowat@gmail.com>
2023-11-12 13:30:38 +1300

    fixed

--- installer/main.go
+++ installer/main.go
@@ -1,40 +1,26 @@
 package main
 
 import (
-	"archive/zip"
 	"fmt"
-	"io"
-	"net/http"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"runtime"
 )
 
 const (
-	zipURL         = "https://proti.suddsy.dev/api/files/x2v7p9msc2kdas2/od4hxwrl3b7kcha/base_a08AmRBEXo.zip?token=" // Replace with your actual URL
-	zipName        = "base.zip"
-	targetDir      = "base"
-	executableName = "base"
+	zipName   = "base.zip"
+	targetDir = "update"
 )
 
 func main() {
-	// Download the zip file
-	err := downloadFile(zipName, zipURL)
-	if err != nil {
-		fmt.Println("Error downloading zip file:", err)
-		return
+	KillOldExe()
+	currentOS := runtime.GOOS
+	executableName := "base"
+	if currentOS == "windows" {
+		executableName = executableName + ".exe"
 	}
-	defer os.Remove(zipName)
-
-	// Unzip the file
-	err = unzip(zipName, targetDir)
-	if err != nil {
-		fmt.Println("Error unzipping file:", err)
-		return
-	}
-
-	// Replace the existing executable with the new one
-	err = replaceExecutable(filepath.Join(targetDir, executableName))
+	err := replaceExecutable(filepath.Join(targetDir, executableName))
 	if err != nil {
 		fmt.Println("Error replacing executable:", err)
 		return
@@ -48,64 +34,17 @@ func main() {
 	}
 
 	fmt.Println("Program executed successfully.")
-}
-
-func downloadFile(filepath string, url string) error {
-	resp, err := http.Get(url)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-
-	file, err := os.Create(filepath)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	_, err = io.Copy(file, resp.Body)
-	return err
-}
-
-func unzip(src, dest string) error {
-	r, err := zip.OpenReader(src)
-	if err != nil {
-		return err
-	}
-	defer r.Close()
-
-	os.MkdirAll(dest, 0755)
-
-	for _, f := range r.File {
-		rc, err := f.Open()
-		if err != nil {
-			return err
-		}
-		defer rc.Close()
-
-		path := filepath.Join(dest, f.Name)
-
-		if f.FileInfo().IsDir() {
-			os.MkdirAll(path, f.Mode())
-		} else {
-			os.MkdirAll(filepath.Dir(path), f.Mode())
-			outFile, err := os.Create(path)
-			if err != nil {
-				return err
-			}
-			defer outFile.Close()
-
-			_, err = io.Copy(outFile, rc)
-			if err != nil {
-				return err
-			}
-		}
-	}
-
-	return nil
+	fmt.Println("Success")
+	os.Exit(0)
 }
 
 func replaceExecutable(newExecutable string) error {
+
+	currentOS := runtime.GOOS
+	executableName := "base"
+	if currentOS == "windows" {
+		executableName = executableName + ".exe"
+	}
 	currentExecutable := filepath.Join("..", executableName)
 	err := os.Rename(newExecutable, currentExecutable)
 	return err
@@ -117,4 +56,37 @@ func runExecutable(executablePath string, args ...string) (*exec.Cmd, error) {
 	cmd.Stderr = os.Stderr
 	err := cmd.Start()
 	return cmd, err
+}
+
+func KillOldExe() {
+	executableName := "base"
+
+	// Get the operating system
+	currentOS := runtime.GOOS
+
+	// Check if it's Windows
+	if currentOS == "windows" {
+		cmd := exec.Command("taskkill", "/F", "/IM", executableName+".exe")
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+
+		// Run the command
+		err := cmd.Run()
+		if err != nil {
+			fmt.Println("Error terminating process:", err)
+		}
+		fmt.Println("Process killed")
+	} else {
+		// Run the pkill command to terminate the process by name
+		cmd := exec.Command("pkill", executableName)
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+
+		// Run the command
+		err := cmd.Run()
+		if err != nil {
+			fmt.Println("Error terminating process:", err)
+		}
+		fmt.Println("Process killed")
+	}
 }
--- updater.go
+++ updater.go
@@ -11,16 +11,56 @@ import (
 	"runtime"
 )
 
-const zipURL = "https://raw.githubusercontent.com/jfmow/pocketbase/master/base.zip" // Replace with your actual URL
+const (
+	zipURL    = "https://github.com/jfmow/pocketbase/raw/main/base.zip" // Replace with your actual URL
+	zipName   = "base.zip"
+	targetDir = "update"
+)
 
-func downloadFile(url, filePath string) error {
+func updater() {
+	// Download the zip file
+	err := downloadFile(zipName, zipURL)
+	if err != nil {
+		fmt.Println("Error downloading zip file:", err)
+		return
+	}
+	defer os.Remove(zipName)
+
+	// Unzip the file
+	err = unzip(zipName, targetDir)
+	if err != nil {
+		fmt.Println("Error unzipping file:", err)
+		return
+	}
+
+	// Run the updated executable
+
+	currentOS := runtime.GOOS
+	executableName := "installer"
+	if currentOS == "windows" {
+		executableName = executableName + ".exe"
+	}
+	fmt.Println(executableName)
+
+	_, err = runExecutable(filepath.Join(targetDir, executableName))
+	if err != nil {
+		fmt.Println("Error running executable:", err)
+		return
+	}
+
+	fmt.Println("Program executed successfully.")
+	fmt.Println("Shuting down...")
+	os.Exit(0)
+}
+
+func downloadFile(filepath string, url string) error {
 	resp, err := http.Get(url)
 	if err != nil {
 		return err
 	}
 	defer resp.Body.Close()
 
-	file, err := os.Create(filePath)
+	file, err := os.Create(filepath)
 	if err != nil {
 		return err
 	}
@@ -30,116 +70,66 @@ func downloadFile(url, filePath string) error {
 	return err
 }
 
-func unzipFile(zipPath, destPath string) error {
-	r, err := zip.OpenReader(zipPath)
+func unzip(src, dest string) error {
+	r, err := zip.OpenReader(src)
 	if err != nil {
 		return err
 	}
 	defer r.Close()
+
+	os.MkdirAll(dest, 0755)
 
 	for _, f := range r.File {
 		rc, err := f.Open()
 		if err != nil {
 			return err
 		}
+		defer rc.Close()
 
-		filePath := filepath.Join(destPath, f.Name)
+		path := filepath.Join(dest, f.Name)
+
 		if f.FileInfo().IsDir() {
-			os.MkdirAll(filePath, os.ModePerm)
+			os.MkdirAll(path, f.Mode())
 		} else {
-			os.MkdirAll(filepath.Dir(filePath), os.ModePerm)
-			outFile, err := os.Create(filePath)
+			os.MkdirAll(filepath.Dir(path), f.Mode())
+			outFile, err := os.Create(path)
 			if err != nil {
 				return err
 			}
 			defer outFile.Close()
+
 			_, err = io.Copy(outFile, rc)
 			if err != nil {
 				return err
 			}
 		}
-		rc.Close()
 	}
+
 	return nil
 }
 
-func restart() {
-	execPath, err := os.Executable()
-	if err != nil {
-		fmt.Println("Error getting executable path:", err)
-		return
+func replaceExecutable(newExecutable string) error {
+	currentOS := runtime.GOOS
+	executableName := "installer"
+	if currentOS == "windows" {
+		executableName = executableName + ".exe"
 	}
-
-	cmd := exec.Command(execPath)
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	cmd.Stdin = os.Stdin
-
-	err = cmd.Start()
-	if err != nil {
-		fmt.Println("Error restarting process:", err)
-		return
-	}
-
-	// Gracefully shut down the old process
-	os.Exit(0)
+	currentExecutable := filepath.Join("..", executableName)
+	err := os.Rename(newExecutable, currentExecutable)
+	return err
 }
 
-func updater() {
-	execPath, err := os.Executable()
-	if err != nil {
-		fmt.Println("Error getting executable path:", err)
-		return
+func runExecutable(executablePath string, args ...string) (*exec.Cmd, error) {
+	cmd := exec.Command(executablePath, args...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	var err error
+
+	currentOS := runtime.GOOS
+	if currentOS != "windows" {
+		err = cmd.Start()
+	} else {
+		cmd.Run()
 	}
-
-	zipPath := filepath.Join(os.TempDir(), "update.zip")
-	err = downloadFile(zipURL, zipPath)
-	if err != nil {
-		fmt.Println("Error downloading zip file:", err)
-		return
-	}
-
-	tempDir := filepath.Join(os.TempDir(), "update")
-	os.RemoveAll(tempDir)
-	os.Mkdir(tempDir, os.ModePerm)
-
-	err = unzipFile(zipPath, tempDir)
-	if err != nil {
-		fmt.Println("Error unzipping file:", err)
-		return
-	}
-
-	baseProgramPath := filepath.Join(tempDir, "base")
-
-	// Platform-specific executable extension
-	executableExt := ""
-	if runtime.GOOS == "windows" {
-		executableExt = ".exe"
-	}
-
-	// Rename the executable
-	newExecPath := execPath + "_new" + executableExt
-	err = os.Rename(baseProgramPath, newExecPath)
-	if err != nil {
-		fmt.Println("Error replacing executable:", err)
-		return
-	}
-
-	// Delete the old executable
-	err = os.Remove(execPath)
-	if err != nil {
-		fmt.Println("Error removing old executable:", err)
-		return
-	}
-
-	// Rename the new executable to the original name
-	err = os.Rename(newExecPath, execPath)
-	if err != nil {
-		fmt.Println("Error renaming new executable:", err)
-		return
-	}
-
-	fmt.Println("Update successful. Restarting...")
-
-	restart()
+	return cmd, err
 }
