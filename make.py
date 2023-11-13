efe72df210936efd6dcd77d5a7de6e5310a7e441
sudsymoss <james1mowat@gmail.com>
2023-11-14 12:46:37 +1300

    upload to pocketbase on build

--- make.py
+++ make.py
@@ -1,43 +1,62 @@
 import os
 import subprocess
-import shutil
-import zipfile
-import paramiko
+import requests
+import sys
 from dotenv import load_dotenv
 load_dotenv()
 # Set environment variables
 os.environ['GOOS'] = 'linux'
-os.environ['GOARCH'] = 'arm'
+if len(sys.argv) == 1 or sys.argv[1] != "noarm":
+    print('Using arm for build')
+    os.environ['GOARCH'] = 'arm'
+
+
 
 # Build the main.go in the current directory and name it 'base'
 subprocess.run(['go', 'build', '-o', 'base', '.'])
 
-# Create a subdirectory 'installer' and build main.go inside it, naming it 'installer'
-#os.chdir('installera')
-#subprocess.run(['go', 'build', '-o', 'installer', '.'])
-#os.chdir('..')
+def uploadToPocketBase(): 
+    url = "https://proti.suddsy.dev/api/collections/devs/auth-with-password"
 
-# Create a zip file 'base.zip' containing 'base' and 'installer'
-#with zipfile.ZipFile('base.zip', 'w') as zipf:
- #   zipf.write('base')
-    #zipf.write(os.path.join('installera', 'installer'), 'installer')
-continueInput = input("Do you wish to upload the base to noti_db_priv? [y/n]\n")    
-if continueInput.lower() == "y":
-    hostname = os.getenv("SSH_IP")
-    username = os.getenv("SSH_USER")
-    remote_path = os.getenv("SSH_PATH")
-    private_key_path = os.getenv('SSH_DIR')
-    passphrase = os.getenv('SSH_KEY')
+    # Set the body parameters
+    body_params = {
+        'identity': os.getenv('DATABASE_DEV_USER'),
+        'password': os.getenv('DATABASE_DEV_PASSWORD')
+    }
 
-    transport = paramiko.Transport((hostname, 698))
-    private_key = paramiko.RSAKey(filename=private_key_path, password=passphrase)
-    transport.connect(username=username, pkey=private_key)
+    # Make the POST request with the body parameters
+    response = requests.post(url, json=body_params)
 
-    sftp = transport.open_sftp_client()
-    sftp.put('base', '/home/pie/pocketbase_docker_scripts/noti_db_priv/base')
+    # Check the response
+    if response.status_code == 200:
+        # Assuming the response is in JSON format and contains a 'token' key
+        response_json = response.json()
+        if 'token' in response_json:
+            print("Signed in!")
+        else:
+            print("Token not found in the response.")
+    else:
+        print("Error:", response.status_code)
+        print(response.text)
 
-    sftp.close()
+    # Set the API endpoint where you want to post the file
+    url = "https://proti.suddsy.dev/api/collections/pocketbases/records"
 
-    transport.close()
+    # Specify the file you want to upload
+    files = {'base': ('base', open('base', 'rb'))}
+
+    # Make the POST request with the files parameter
+    response2 = requests.post(url, files=files, headers={'Authorization': response_json['token']})
+
+    # Check the response
+    if response2.status_code == 200:
+        print("File uploaded successfully")
+    else:
+        print("Error uploading file. Status code:", response2.status_code)
+        print(response.text)
+
+continueInput2 = input("Do you wish to upload the base to the database? [y/n]\n")    
+if continueInput2.lower() == "y":
+    uploadToPocketBase()
 
 print("Build and packaging completed.")
